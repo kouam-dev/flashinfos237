d1cf4a759f0efc6c9f48d7bc7c0a1e2c5fa76ccf
parfait-code <kouamparait5@gmail.com>
2025-05-01 13:22:53 +0100

    refactor: update manifest description and correct sitemap/feed URLs in robots.txt

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,7 +4,7 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Flash Infos 237',
     short_name: 'flashinfos237',
-    description: "Découvrez les dernières actualités et articles de votre source d'information préférée",
+    description: "Découvrez les dernières actualités du cameroun et articles de votre source d'information préférée",
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
